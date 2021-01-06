2ebcd61eba88d0952a7957e49776907115f3e816
Jatin Ambasana <jatinambasana@gmail.com>
2021-01-06 14:09:07 +0530

    Programs updated

--- AJP_Program01/src/java/Servlet1.java
+++ AJP_Program01/src/java/Servlet1.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Servlet1 extends HttpServlet {
 
-protected void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
+public void doGet(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException 
 {
     response.setContentType("text/html");
     PrintWriter out = response.getWriter();  
