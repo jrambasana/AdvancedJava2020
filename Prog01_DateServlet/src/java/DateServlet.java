472a7a4dbe27395b654fe7a4609ad7a46acb16de
Jatin Ambasana <jatinambasana@gmail.com>
2021-02-08 10:58:22 +0530

    GTU - Servlet Practical 1 and 2 added

---
+++ Prog01_DateServlet/src/java/DateServlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DateServlet extends HttpServlet {
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    
+    out.println("Hello ! Today is: " + new Date());
+}
+}
